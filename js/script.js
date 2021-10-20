b6bd75cbe2be29a170b8251f681c0a61873740b3
Giadaspt <saputo.giada@gmail.com>
2021-10-20 15:41:30 +0200

    bonus conta le lettere della password

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 
-
-
 const firstname = prompt('Qual è il tuo nome?');
 const surname = prompt('Ed il tuo cognome?');
 const prefColor = prompt('Qual è il tuo colore preferito?');
 const endYear = parseInt('21');
 const details = firstname+surname+prefColor+endYear;
+let totLetter = details.length;
 
 console.log(firstname);
 console.log(surname);
@@ -16,4 +15,9 @@ console.log(details);
 document.getElementById('details').innerHTML = 
 `
   ${details}
+`;
+
+document.getElementById('password').innerHTML = 
+`
+  ${totLetter}
 `;
