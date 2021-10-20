15f618ff405060b7bdd1fec8f8d6ea9b96c030d6
Giadaspt <saputo.giada@gmail.com>
2021-10-20 15:24:49 +0200

    password

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 
+
+
+const firstname = prompt('Qual è il tuo nome?');
+const surname = prompt('Ed il tuo cognome?');
+const prefColor = prompt('Qual è il tuo colore preferito?');
+const endYear = parseInt('21');
+const details = firstname+surname+prefColor+endYear
+
+console.log(firstname);
+console.log(surname);
+console.log(prefColor);
+
+document.getElementById('details').innerHTML = (firstname+surname+prefColor+endYear);
+
+
+document.getElementById('password').innerHTML = 
+`
+  Password: ${firstname}${surname}${prefColor};
+`;
